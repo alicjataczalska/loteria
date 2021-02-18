f5d28c0db7ed6feb78c93a9faab9206fa4d0d133
alicjataczalska <61902914+alicjataczalska@users.noreply.github.com>
2021-02-18 13:57:01 +0100

    Add files via upload

---
+++ loteria/loteria/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace loteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.narzędziaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wygenerujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.czyśćToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oautorzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opisToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.zamknijToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.wygenerujToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.czyśćToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.oAutorzeToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.opisToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(32, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(187, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Wygeneruj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(100, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 29);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(173, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(46, 29);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(247, 81);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(46, 29);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(322, 81);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(46, 29);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox6.Location = new System.Drawing.Point(387, 81);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(46, 29);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label1.Location = new System.Drawing.Point(181, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 38);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lotto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.narzędziaToolStripMenuItem,
+            this.opisToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(468, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // narzędziaToolStripMenuItem
+            // 
+            this.narzędziaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zamknijToolStripMenuItem,
+            this.wygenerujToolStripMenuItem,
+            this.czyśćToolStripMenuItem});
+            this.narzędziaToolStripMenuItem.Name = "narzędziaToolStripMenuItem";
+            this.narzędziaToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.narzędziaToolStripMenuItem.Text = "Narzędzia";
+            this.narzędziaToolStripMenuItem.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zamknijToolStripMenuItem.Text = "&Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // wygenerujToolStripMenuItem
+            // 
+            this.wygenerujToolStripMenuItem.Name = "wygenerujToolStripMenuItem";
+            this.wygenerujToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.wygenerujToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.wygenerujToolStripMenuItem.Text = "&Wygeneruj";
+            this.wygenerujToolStripMenuItem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // czyśćToolStripMenuItem
+            // 
+            this.czyśćToolStripMenuItem.Name = "czyśćToolStripMenuItem";
+            this.czyśćToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.czyśćToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.czyśćToolStripMenuItem.Text = "&Czyść";
+            this.czyśćToolStripMenuItem.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // opisToolStripMenuItem
+            // 
+            this.opisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oautorzeToolStripMenuItem,
+            this.opisToolStripMenuItem1});
+            this.opisToolStripMenuItem.Name = "opisToolStripMenuItem";
+            this.opisToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.opisToolStripMenuItem.Text = "Opis";
+            // 
+            // oautorzeToolStripMenuItem
+            // 
+            this.oautorzeToolStripMenuItem.Name = "oautorzeToolStripMenuItem";
+            this.oautorzeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.oautorzeToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.oautorzeToolStripMenuItem.Text = "O &Autorze";
+            this.oautorzeToolStripMenuItem.Click += new System.EventHandler(this.oautorzeToolStripMenuItem_Click);
+            // 
+            // opisToolStripMenuItem1
+            // 
+            this.opisToolStripMenuItem1.Name = "opisToolStripMenuItem1";
+            this.opisToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.opisToolStripMenuItem1.Size = new System.Drawing.Size(182, 22);
+            this.opisToolStripMenuItem1.Text = "O &programie";
+            this.opisToolStripMenuItem1.Click += new System.EventHandler(this.opisToolStripMenuItem1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Zamknij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(256, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Czyść";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zamknijToolStripMenuItem1,
+            this.wygenerujToolStripMenuItem1,
+            this.czyśćToolStripMenuItem1,
+            this.oAutorzeToolStripMenuItem1,
+            this.opisToolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(183, 114);
+            // 
+            // zamknijToolStripMenuItem1
+            // 
+            this.zamknijToolStripMenuItem1.Name = "zamknijToolStripMenuItem1";
+            this.zamknijToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.zamknijToolStripMenuItem1.Size = new System.Drawing.Size(182, 22);
+            this.zamknijToolStripMenuItem1.Text = "&Zamknij";
+            this.zamknijToolStripMenuItem1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // wygenerujToolStripMenuItem1
+            // 
+            this.wygenerujToolStripMenuItem1.Name = "wygenerujToolStripMenuItem1";
+            this.wygenerujToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.wygenerujToolStripMenuItem1.Size = new System.Drawing.Size(182, 22);
+            this.wygenerujToolStripMenuItem1.Text = "&Wygeneruj";
+            this.wygenerujToolStripMenuItem1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // czyśćToolStripMenuItem1
+            // 
+            this.czyśćToolStripMenuItem1.Name = "czyśćToolStripMenuItem1";
+            this.czyśćToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.czyśćToolStripMenuItem1.Size = new System.Drawing.Size(182, 22);
+            this.czyśćToolStripMenuItem1.Text = "&Czyść";
+            this.czyśćToolStripMenuItem1.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // oAutorzeToolStripMenuItem1
+            // 
+            this.oAutorzeToolStripMenuItem1.Name = "oAutorzeToolStripMenuItem1";
+            this.oAutorzeToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.oAutorzeToolStripMenuItem1.Size = new System.Drawing.Size(182, 22);
+            this.oAutorzeToolStripMenuItem1.Text = "O &Autorze";
+            this.oAutorzeToolStripMenuItem1.Click += new System.EventHandler(this.oAutorzeToolStripMenuItem1_Click);
+            // 
+            // opisToolStripMenuItem2
+            // 
+            this.opisToolStripMenuItem2.Name = "opisToolStripMenuItem2";
+            this.opisToolStripMenuItem2.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.opisToolStripMenuItem2.Size = new System.Drawing.Size(182, 22);
+            this.opisToolStripMenuItem2.Text = "O &programie";
+            this.opisToolStripMenuItem2.Click += new System.EventHandler(this.opisToolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(468, 212);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lotto";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem narzędziaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wygenerujToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oautorzeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opisToolStripMenuItem1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem czyśćToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem wygenerujToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem czyśćToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem oAutorzeToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem opisToolStripMenuItem2;
+    }
+}
+
---
+++ loteria/loteria/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loteria
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        int val = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generatorNumerow();
+
+        }
+
+        private void generatorNumerow()
+        {
+            val = rand.Next(1, 49);
+            textBox1.Text = val.ToString();
+
+            val = rand.Next(1, 49);
+            while(val.ToString().Equals(textBox1.Text))
+                val = rand.Next(1, 49);
+            textBox2.Text = val.ToString();
+
+
+            val = rand.Next(1, 49);
+            while (val.ToString().Equals(textBox1.Text) || val.ToString().Equals(textBox2.Text))
+                val = rand.Next(1, 49);
+            textBox3.Text = val.ToString();
+
+            val = rand.Next(1, 49);
+            while (val.ToString().Equals(textBox1.Text) || val.ToString().Equals(textBox2.Text) || val.ToString().Equals(textBox3.Text))
+                val = rand.Next(1, 49);
+            textBox4.Text = val.ToString();
+
+            val = rand.Next(1, 49);
+            while (val.ToString().Equals(textBox1.Text) || val.ToString().Equals(textBox2.Text) || val.ToString().Equals(textBox3.Text) || val.ToString().Equals(textBox4.Text))
+                val = rand.Next(1, 49);
+            textBox5.Text = val.ToString();
+
+            val = rand.Next(1, 49);
+            while (val.ToString().Equals(textBox1.Text) || val.ToString().Equals(textBox2.Text) || val.ToString().Equals(textBox3.Text) || val.ToString().Equals(textBox4.Text) || val.ToString().Equals(textBox5.Text))
+                val = rand.Next(1, 49);
+            textBox6.Text = val.ToString();
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            textBox3.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void oautorzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Alicja Taczalska II Bg");
+        }
+
+        private void opisToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Program losuje sześć liczb w zakresie od 1 do 49");
+        }
+
+        private void oAutorzeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Alicja Taczalska II Bg");
+        }
+
+        private void opisToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Program losuje sześć liczb w zakresie od 1 do 49");
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
